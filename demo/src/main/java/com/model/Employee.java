01df894ae0389b95e2c49cc7ff5e2f804cb544e9
johntvu <120274177+johnvu124@users.noreply.github.com>
2025-04-21 15:37:01 -0400

    Create folder to store models

---
+++ demo/src/main/java/com/model/Employee.java
@@ -0,0 +1,37 @@
+package com.model;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String division;
+
+    public Employee(int id, String name, String email, String jobTitle, String division) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.jobTitle = jobTitle;
+        this.division = division;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+}
